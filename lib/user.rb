385592ed403be8032b1df5e0fde8a3ea8bc6b57a
Kaitlin Forsman <kcforsman@outlook.com>
2018-03-05 13:26:26 -0800

    stubbed some tests for user class and wrote tests and code for user initialize method approach

--- lib/user.rb
+++ lib/user.rb
@@ -1,6 +1,14 @@
 require 'date'
+require 'pry'
 
 module Hotel
+  TOTAL_ROOMS = 20
+  COST_PER_NIGHT = 200
   class User
+    attr_reader :rooms
+    def initialize
+      @rooms = []
+      TOTAL_ROOMS.times { | room_num | @rooms << room_num + 1}
+    end
   end
 end
--- specs/user_spec.rb
+++ specs/user_spec.rb
@@ -1,2 +1,33 @@
 require_relative 'spec_helper'
 require 'date'
+
+describe 'User' do
+  describe 'initialization' do
+    before do
+      @admin = Hotel::User.new
+    end
+    it 'can be initialized' do
+      @admin.must_be_instance_of Hotel::User
+    end
+    it 'has a list of rooms' do
+      # hotel_rooms = @admin.rooms
+
+      @admin.must_respond_to :rooms
+    end
+  end
+  xdescribe 'reserve_room' do
+    it 'throws StandardError for invalide dates' do
+    end
+    it 'returns an instance of a reservation' do
+    end
+  end
+  xdescribe 'find_reservations_for_given_date' do
+    it 'returns array of reservations for a given date' do
+    end
+  end
+  xdescribe 'calculate_reservation_cost' do
+    it 'returns cost for reservation' do
+    end
+  end
+
+end
