b9f7f432b685f5f46bebb4922702a1cb0e1b1b11
Kaitlin Forsman <kcforsman@outlook.com>
2018-03-05 14:21:35 -0800

    added errors for invalid inputs to reserve_room method

--- lib/user.rb
+++ lib/user.rb
@@ -10,5 +10,14 @@ module Hotel
       @rooms = []
       TOTAL_ROOMS.times { | room_num | @rooms << room_num + 1}
     end
+    def reserve_room(guest, start_date, end_date)
+      if start_date.class != Date || end_date.class != Date
+        raise StandardError.new("not a date")
+      elsif end_date < start_date
+        raise ArgumentError.new("End date (#{end_date}) comes before start date (#{start_date})")
+      elsif end_date == start_date
+        raise ArgumentError.new("End date (#{end_date}) is same as start date (#{start_date})")
+      end
+    end
   end
 end
--- specs/user_spec.rb
+++ specs/user_spec.rb
@@ -10,13 +10,22 @@ describe 'User' do
       @admin.must_be_instance_of Hotel::User
     end
     it 'has a list of rooms' do
-      # hotel_rooms = @admin.rooms
-
       @admin.must_respond_to :rooms
     end
   end
-  xdescribe 'reserve_room' do
-    it 'throws StandardError for invalide dates' do
+  describe 'reserve_room' do
+    before do
+      @admin = Hotel::User.new
+    end
+    it 'throws StandardError for invalidate dates' do
+      proc { @admin.reserve_room("Jade Poe", nil, Date.new(2018,3,5))}.must_raise StandardError
+      proc { @admin.reserve_room("Jade Poe", Date.new(2018,3,5), nil)}.must_raise StandardError
+    end
+    it 'throws ArgumentError if end_date occurs before start_date' do
+      proc { @admin.reserve_room("Jade Poe", Date.new(2018, 3, 10), Date.new(2018, 3, 5))}.must_raise ArgumentError
+    end
+    it 'throws ArgumentError if start_date is same as end_date' do
+      proc { @admin.reserve_room("Jade Poe", Date.new(2018, 3, 10), Date.new(2018, 3, 10))}.must_raise ArgumentError
     end
     it 'returns an instance of a reservation' do
     end
