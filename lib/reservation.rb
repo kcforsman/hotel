31315fbe7fd82d6344f8cdafa20b9b56ad728d62
Kaitlin Forsman <kcforsman@outlook.com>
2018-03-05 15:07:06 -0800

    initialized reservation class with attributes: id, room_num, guest, start_date, end_date

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -2,5 +2,13 @@ require 'date'
 
 module Hotel
   class Reservation
+    attr_reader :id, :room_num, :guest, :start_date, :end_date
+    def initialize id, room_num, guest, start_date, end_date
+      @id = id
+      @room_num = room_num
+      @guest = guest
+      @start_date = start_date
+      @end_date = end_date
+    end
   end
 end
--- specs/reservation_spec.rb
+++ specs/reservation_spec.rb
@@ -1,2 +1,28 @@
 require_relative 'spec_helper'
 require 'date'
+
+describe 'Reservation' do
+  before do
+    @reservation = Hotel::Reservation.new(1, 4, "Bob", Date.new(2018,3,26), Date.new(2018,3,18))
+  end
+  describe 'initialization' do
+    it 'can be initialized' do
+      @reservation.must_be_instance_of Hotel::Reservation
+    end
+    it 'has attributes: id, room_num, guest, start_date, end_date' do
+      @reservation.id.must_equal 1
+      @reservation.id.must_be_kind_of Integer
+      @reservation.room_num.must_equal 4
+      @reservation.room_num.must_be_kind_of Integer
+      @reservation.guest.must_equal "Bob"
+      @reservation.guest.must_be_kind_of String
+      @reservation.start_date.must_be_instance_of Date
+      @reservation.end_date.must_be_instance_of Date
+    end
+  end
+  describe 'calculate_nights' do
+    it 'returns the number of nights' do
+
+    end
+  end
+end
